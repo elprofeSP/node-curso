6b0d67973a6b68d43890d91aa5016aaec8af6f1c
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-08-24 15:05:00 -0500

    Modularizar codigo en nodejs

--- 01-modularizar/esmodules/index.mjs
+++ 01-modularizar/esmodules/index.mjs
@@ -5,5 +5,5 @@ saludar(estudiantes[0]);
 despedirse(estudiantes[2]);
 
 // .js
-// .mjs - modulos de ecma script
-// .cjs - common js
+// .mjs - modulos de ecma script -> import / export
+// .cjs - common js -> require / module.exports
