4056e90e1e8e2183afe085606af8b689332999dc
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-08-31 16:36:53 -0500

    Obtener usuario por su id

--- 04-api-rest/app.js
+++ 04-api-rest/app.js
@@ -23,8 +23,15 @@ app.get("/users", (req, res) => {
 // Obtener un usuario por su id
 app.get("/users/:id", (req, res) => {
   // Obtener el id
-  const id = req.params.id;
-  res.send(`Usuario ${id}`);
+  const id = parseInt(req.params.id);
+  // Buscar el usuario con el mismo id que llaga por parametro con el metodo find
+  const user = users.find((user) => user.id === id);
+  // Si no hay usuario
+  if (!user) {
+    return res.status(404).json({ error: "Usuario no encontrado" });
+  }
+  // Si hay usuario
+  return res.json(user);
 });
 
 // Ruta no encontrada
--- 04-api-rest/utils.js
+++ 04-api-rest/utils.js
@@ -1,3 +1,29 @@
 import { createRequire } from "node:module";
 // Crear nuestro propio require
 export const require = createRequire(import.meta.url);
+
+// const users = [
+//   {
+//     id: 1,
+//     name: "Jonathan",
+//     email: "jonathan@gmail.com",
+//   },
+//   {
+//     id: 1,
+//     name: "Angelo",
+//     email: "angelo@gmail.com",
+//   },
+//   {
+//     id: 3,
+//     name: "Jose",
+//     email: "jose@gmail.com",
+//   },
+// ];
+
+// const usuarioEncontrado = users.find((user) => user.id === 1 );
+// console.log(usuarioEncontrado)
+
+// const numeros = [ 2, 7, 6, 8]
+
+// const numeroEncontrado = numeros.find((numero) => numero === 7)
+// console.log(numeroEncontrado)
