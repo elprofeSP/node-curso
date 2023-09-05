35cc7d5b6e4b6fec64476cce18739d6051f4bfd6
Fabian Gomez <fabian.gomez@somospacifico.org>
2023-09-05 16:29:29 -0500

    Ejercicio finalizado

--- 04-api-rest/app.js
+++ 04-api-rest/app.js
@@ -1,8 +1,9 @@
-// import { randomUUID } from "node:crypto"; -> otra opcion de generar ids
+import { randomUUID } from "node:crypto";
 import express from "express";
 import { require } from "./utils.js";
 
 const users = require("./users.json");
+const movies = require("./movies.json");
 
 const app = express();
 
@@ -90,6 +91,74 @@ app.delete("/users/:id", (req, res) => {
     // Responder con un mensaje
     res.json({ message: "Usuario eliminado correctamente" });
   }
+});
+
+// Obtener todas la peliculas
+app.get("/movies", (req, res) => {
+  if (req.query.year) {
+    const peliculasFiltradas = movies.filter(
+      (movie) => movie.year == req.query.year
+    );
+    if (peliculasFiltradas.length === 0) {
+      return res.status(404).json({ error: "No hay peliculas" });
+    }
+    return res.json(peliculasFiltradas);
+  }
+  if (movies.length === 0) {
+    return res.status(404).json({ error: "No hay peliculas" });
+  }
+  res.json(movies);
+});
+
+// Obtener pelicula por su id
+app.get("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  const movie = movies.find((movie) => movie.id === id);
+  if (!movie) {
+    return res.status(404).json({ error: "Pelicula no encontrada" });
+  }
+  return res.json(movie);
+});
+
+// Crear una pelicula
+app.post("/movies", (req, res) => {
+  if (!req.body.title || !req.body.year) {
+    return res.status(400).json({ error: "Error en la peticion" });
+  }
+  const movie = req.body;
+  movie.id = randomUUID();
+  movies.push(movie);
+  return res.status(201).json(movie);
+});
+
+// Modificar una pelicula
+app.patch("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  // Encontrar el indice de la pelicula en la db por su id
+  const index = movies.findIndex((movie) => movie.id === id);
+  // Si no existe ninguna pelicula con ese id
+  if (index === -1) {
+    return res.status(404).json({ error: "Pelicula no encontrada" });
+  } else {
+    const movieUpdated = { ...movies[index], ...req.body };
+    movies[index] = movieUpdated;
+    res.json(movieUpdated);
+  }
+});
+
+// Borrar una pelicula
+app.delete("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  // Encontrar el indice de la pelicula en la db por su id
+  const index = movies.findIndex((movie) => movie.id === id);
+  // Si no existe ninguna pelicula con ese id
+  if (index === -1) {
+    return res.status(404).json({ error: "Pelicula no encontrada" });
+  }
+  // Eliminar la pelicula de la base de datos segun su id
+  movies.splice(index, 1);
+  // Responder con estado
+  res.sendStatus(204);
 });
 
 // Ruta no encontrada
